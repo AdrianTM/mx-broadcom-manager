3342bc9cd6a1c065d9c4a67403d812642c20959a
oldgizahub <geoffgigg@hotmail.com>
2016-11-29 11:15:49 -0500

    new French translations for mx-broadcom-manager

--- translations/mx-broadcom-manager_fr.ts
+++ translations/mx-broadcom-manager_fr.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
 <context>
     <name>MConfig</name>
     <message>
@@ -50,7 +48,7 @@
     <message>
         <location filename="../mconfig.cpp" line="378"/>
         <source>Traceroute is not installed, do you want to install it now?</source>
-        <translation>Traceroute n&apos;est pas installé. Voulez-vous maintenant l&apos;installer?</translation>
+        <translation>Traceroute n&apos;est pas installé. Voulez-vous l&apos;installer maintenant?</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="386"/>
@@ -60,12 +58,12 @@
     <message>
         <location filename="../mconfig.cpp" line="387"/>
         <source>Traceroute cannot be installed. This may mean you are using the LiveCD or you are unable to reach the software repository,</source>
-        <translation>Impossible d&apos;installer Traceroute. Il se peut que vous êtes en train d&apos;utiliser le CD-ROM autonome, sinon on ne peut pas accéder au référentiel logiciel.</translation>
+        <translation>Impossible d&apos;installer Traceroute. Cela se produit si vous utilisez un Live CD ou si vous n&apos;êtes pas en mesure d&apos;accéder au dépôt logiciel.</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="400"/>
         <source>Traceroute is not installed and no Internet connection could be detected so it cannot be installed</source>
-        <translation>Traceroute n&apos;est pas déjà installé. De plus, on ne peut pas l&apos;installer maintenant parce que la connexion à Internet ne fonctionne pas.</translation>
+        <translation>Traceroute n&apos;est pas encore installé et son installation est impossible car aucune connexion à Internet n&apos;a été détectée.</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="407"/>
@@ -87,27 +85,27 @@
     <message>
         <location filename="../mconfig.cpp" line="1095"/>
         <source>Windows installation information file (*.inf)</source>
-        <translation>Fichier d&apos;information pour l&apos;installation sur Windows (*.inf)</translation>
+        <translation>Fichier d&apos;information pour installation Windows (*.inf)</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1127"/>
         <source>*.sys file not found</source>
-        <translation>Fichier de type *.sys pas trouvé</translation>
+        <translation>Fichier *.sys introuvable</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1127"/>
         <source>The *.sys files must be in the same location as the *.inf file. %1 cannot be found</source>
-        <translation>Les fichiers de type *.sys doivent se trouver en même emplacement que le fichier de type *.inf. %1 pas trouvé</translation>
+        <translation>Les fichiers *.sys doivent se trouver dans le même emplacement que le fichier *.inf. %1 introuvable.</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1138"/>
         <source>sys file reference not found</source>
-        <translation>Impossible de trouver le fichier de référence de type sys</translation>
+        <translation>Impossible de trouver le fichier de référence sys</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1138"/>
         <source>The sys file for the given driver cannot be determined after parsing the inf file</source>
-        <translation>Apres avoir analysé le fichier de type inf, on ne peut pas déterminer le fichier de type sys à  l&apos;intention du pilote convenu</translation>
+        <translation>Apres analyse du fichier inf, il est impossible de déterminer le fichier sys du pilote donné</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1157"/>
@@ -117,12 +115,12 @@
     <message>
         <location filename="../mconfig.cpp" line="1206"/>
         <source>About MX Broadcom Manager</source>
-        <translation>À propos de MX Gestionnaire du Broadcom</translation>
+        <translation>À propos de MX Gestionnaire de carte réseau Broadcom</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1207"/>
         <source>MX Broadcom Manager</source>
-        <translation>MX Gestionnaire de carte réseau  Broadcom</translation>
+        <translation>MX Gestionnaire de carte réseau Broadcom</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1207"/>
@@ -132,7 +130,7 @@
     <message>
         <location filename="../mconfig.cpp" line="1209"/>
         <source>Program for troubleshooting and configuring network for MX Linux</source>
-        <translation>Programme pour le dépannage et la configuration du réseau sur MX Linux</translation>
+        <translation>Programme permettant le dépannage et la configuration réseau dans MX Linux</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1210"/>
@@ -142,7 +140,7 @@
     <message>
         <location filename="../mconfig.cpp" line="1211"/>
         <source>License</source>
-        <translation>License</translation>
+        <translation>Licence</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1212"/>
@@ -155,7 +153,7 @@
     <message>
         <location filename="../meconfig.ui" line="26"/>
         <source>MX Broadcom Manager</source>
-        <translation>MX Gestionnaire du Broadcom</translation>
+        <translation>MX Gestionnaire de carte réseau Broadcom</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="80"/>
@@ -186,7 +184,7 @@
     <message>
         <location filename="../meconfig.ui" line="210"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Broadcom wireless can sometimes be difficult to set up in Linux. There are a number of different types of drivers that have different capabilities and features.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;L&apos;initialisation dans Linux de l&apos;adapteur sans fil Broadcom se trouve parfois difficile à effectuer. Il y a beaucoup de pilotes de sortes différentes et avec des capabilités et fonctions divers.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;La configuration des adaptateurs sans fil Broadcom est parfois difficile sous Linux. Il existe de nombreux types de pilotes ayant différentes capacités et fonctionnalités.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="226"/>
@@ -196,12 +194,12 @@
     <message>
         <location filename="../meconfig.ui" line="244"/>
         <source>Re-scan Hardware</source>
-        <translation>Balayer de nouveau le matériel</translation>
+        <translation>Rebalayer le matériel</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="332"/>
         <source>Unblocks all soft/hard blocked wireless devices</source>
-        <translation>Débloquer tous les périphériques WiFi bloqués (si doux ou dur)</translation>
+        <translation>Débloquer tous les périphériques WiFi bloqués</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="335"/>
@@ -212,17 +210,17 @@
         <location filename="../meconfig.ui" line="354"/>
         <location filename="../meconfig.ui" line="360"/>
         <source>Linux drivers</source>
-        <translation>Les pilotes Linux</translation>
+        <translation>Pilotes Linux</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="366"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;This tool makes it easier to set up the network driver for your Broadcom network card.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Cet outil rend plus facile l&apos;initialisation du pilote de réseau à  l&apos;intention de votre carte Broadcom.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Cet outil facilite l&apos;initialisation du pilote réseau de votre carte Broadcom.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="385"/>
         <source>Associated Linux drivers</source>
-        <translation>Les pilotes associés Linux </translation>
+        <translation>Pilotes Linux associés</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="394"/>
@@ -237,7 +235,7 @@
     <message>
         <location filename="../meconfig.ui" line="474"/>
         <source>Re-list Linux Drivers</source>
-        <translation>Énumérer de nouveau les pilotes Linux</translation>
+        <translation>Relister les pilotes Linux</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="494"/>
@@ -262,7 +260,7 @@
     <message>
         <location filename="../meconfig.ui" line="542"/>
         <source>In order to use Windows drivers you need first to install NDISwrapper, please press the Install NDISwrapper button.</source>
-        <translation>Pour se servir des pilotes Windows, on doit d&apos;abord installer NDISwrapper. Veuillez appuyer sur le bouton Installer NDISwrapper.</translation>
+        <translation>Pour utiliser les pilotes Windows, NDISwrapper doit d&apos;abord être installé. Veuillez appuyer sur le bouton Installer NDISwrapper.</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="558"/>
@@ -287,12 +285,12 @@
     <message>
         <location filename="../meconfig.ui" line="640"/>
         <source>List Windows drivers</source>
-        <translation>Énumerer les pilotes Windows</translation>
+        <translation>Lister les pilotes Windows</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="663"/>
         <source>Net diagnostics</source>
-        <translation>Les diagnostics du réseau</translation>
+        <translation>Diagnostics réseau</translation>
     </message>
     <message>
         <location filename="../meconfig.ui" line="669"/>
@@ -348,7 +346,7 @@
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Sans'; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
@@ -377,7 +375,7 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../mconfig.cpp" line="683"/>
         <source> and in use by </source>
-        <translation>et en service de</translation>
+        <translation>et utilisé par</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="692"/>
@@ -387,23 +385,23 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../mconfig.cpp" line="784"/>
         <source>Driver removed from blacklist</source>
-        <translation>On a retiré le pilote de liste noire</translation>
+        <translation>Le pilote a été retiré de la liste noire</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="785"/>
         <source>Driver removed from blacklist.</source>
-        <translation>On a retiré le pilote de liste noire.</translation>
+        <translation>Le pilote a été retiré de la liste noire.</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="793"/>
         <location filename="../mconfig.cpp" line="794"/>
         <source>Module blacklisted</source>
-        <translation>On a mis le module sur liste noire</translation>
+        <translation>Le module a été mis sur liste noire</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="987"/>
         <source>Installation successful</source>
-        <translation>L&apos;installation a réussi</translation>
+        <translation>Installation réussie</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="991"/>
@@ -418,12 +416,12 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../mconfig.cpp" line="1008"/>
         <source>Error encountered while removing Ndiswrapper</source>
-        <translation>Une erreur s&apos;est produit en enlevant ndiswrapper</translation>
+        <translation>Une erreur s&apos;est produite en supprimant ndiswrapper</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1066"/>
         <source>Unblacklist Driver</source>
-        <translation>Retirer le pilote de liste noire</translation>
+        <translation>Retirer le pilote de la liste noire</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1071"/>
@@ -435,7 +433,7 @@ p, li { white-space: pre-wrap; }
         <source>Could not unlock devices.
 WiFi device(s) might already be unlocked.</source>
         <translation>Impossible de déverrouiller les périphériques.
-Il se peut que les périphériques Wi-Fi sont déjà déverrouillés.</translation>
+Il se peut que les périphériques Wi-Fi soient déjà déverrouillés.</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1190"/>
@@ -446,13 +444,13 @@ Il se peut que les périphériques Wi-Fi sont déjà déverrouillés.</translati
         <location filename="../mconfig.cpp" line="1239"/>
         <location filename="../mconfig.cpp" line="1240"/>
         <source>Driver loaded successfully</source>
-        <translation>Le chargement du pilote a réussi</translation>
+        <translation>Chargement du pilote accompli</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="1258"/>
         <location filename="../mconfig.cpp" line="1259"/>
         <source>Driver unloaded successfully</source>
-        <translation>Le déchargement du pilote a réussi</translation>
+        <translation>Déchargement du pilote accompli</translation>
     </message>
 </context>
 <context>
@@ -460,12 +458,12 @@ Il se peut que les périphériques Wi-Fi sont déjà déverrouillés.</translati
     <message>
         <location filename="../mconfig.cpp" line="813"/>
         <source>Could not load </source>
-        <translation>Impossible de charger</translation>
+        <translation>Chargement impossible</translation>
     </message>
     <message>
         <location filename="../mconfig.cpp" line="849"/>
         <source>Could not unload </source>
-        <translation>Impossible de décharger</translation>
+        <translation>Déchargement impossible</translation>
     </message>
 </context>
 </TS>
